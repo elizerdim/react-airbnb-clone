6003c2126dfab48371f1c09a8aa26368fdf9ecec
Eliz Erdim <elizerdim@gmail.com>
2024-04-28 02:21:20 +0300

    Add App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import './App.css'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
-function App() {
-
+export default function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        {...item}
+      />
+    )
+  })
 
   return (
     <>
-      
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
     </>
   )
 }
-
-export default App
